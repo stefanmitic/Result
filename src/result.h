c7b3dd6dfedc8a414d2f86681f13dc9b304c2c2d
atimilam <atimilam@gmail.com>
2020-03-11 02:03:27 +0100

    Added specializations for int, bool and enum. Anything else should raise an error.

--- src/result.h
+++ src/result.h
@@ -1,3 +1,28 @@
+
+/*
+MIT License
+
+Copyright (c) 2020 stefanmitic
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #ifndef RESULT_H
 #define RESULT_H
 
@@ -10,33 +35,18 @@ struct nullresult_t
 
 inline constexpr nullresult_t nullresult {};
 
-template <typename T>
-class Result
+template<typename C, typename T, typename = void>
+class ResultBase
 {
     public:
-        Result(int code, T value)
-            : m_code(code), m_value(value)
-        {
-        }
+        virtual bool HasValue() const = 0;
 
-        Result(int code, nullresult_t value)
-            : m_code(code)
-        {
-        }
+        virtual constexpr bool operator!() const = 0;
 
-        bool HasValue() const
-        {
-            return m_code >= 0;
-        }
-
+        // General member functions
         T Value() const
         {
             return m_value;
-        }
-
-        constexpr bool operator!() const
-        {
-            return m_code < 0;
         }
 
         constexpr const T* operator->() const
@@ -64,9 +74,90 @@ class Result
             return m_value;
         }
 
-    private:
-        int m_code;
+    protected:
+        C m_code;
         T m_value;
+};
+
+// Enum specialization
+template <typename C, typename T>
+class Result : public ResultBase<C, T, typename std::enable_if_t<std::is_enum_v<C>>>
+{
+    public:
+        Result(C code, T value)
+        {
+            this->m_code = code;
+            this->m_value = value;
+        }
+
+        Result(C code, nullresult_t value)
+        {
+            this->m_code = code;
+        }
+
+        bool HasValue() const
+        {
+            return this->m_code >= 0;
+        }
+
+        constexpr bool operator!() const
+        {
+            return this->m_code < 0;
+        }
+};
+
+// Int specilaization
+template<typename T>
+class Result<int, T> : public ResultBase<int, T>
+{
+    public:
+        Result(int code, T value)
+        {
+            this->m_code = code;
+            this->m_value = value;
+        }
+
+        Result(int code, nullresult_t value)
+        {
+            this->m_code = code;
+        }
+
+        bool HasValue() const
+        {
+            return this->m_code >= 0;
+        }
+
+        constexpr bool operator!() const
+        {
+            return this->m_code < 0;
+        }
+};
+
+// Bool specilaization
+template<typename T>
+class Result<bool, T> : public ResultBase<bool, T>
+{
+    public:
+        Result(bool code, T value)
+        {
+            this->m_code = code;
+            this->m_value = value;
+        }
+
+        Result(bool code, nullresult_t value)
+        {
+            this->m_code = code;
+        }
+
+        bool HasValue() const
+        {
+            return this->m_code;
+        }
+
+        constexpr bool operator!() const
+        {
+            return this->m_code;
+        }
 };
 };
 
--- tests/result_tests.cpp
+++ tests/result_tests.cpp
@@ -26,11 +26,11 @@ class TestClass
 
 TEST(Result, EnumTest)
 {
-    Result::Result<int> worseError = Result::Result(TestEnum::WorseError, 0);
-    Result::Result<int> error = Result::Result(TestEnum::Error, 0);
-    Result::Result<int> ok = Result::Result(TestEnum::Ok, 2);
-    Result::Result<int> stillOk = Result::Result(TestEnum::StillOk, 3);
-    Result::Result<int> evenMoreOk = Result::Result(TestEnum::EvenMoreOk, 4);
+    Result::Result<TestEnum, int> worseError = Result::Result(TestEnum::WorseError, 0);
+    Result::Result<TestEnum, int> error = Result::Result(TestEnum::Error, 0);
+    Result::Result<TestEnum, int> ok = Result::Result(TestEnum::Ok, 2);
+    Result::Result<TestEnum, int> stillOk = Result::Result(TestEnum::StillOk, 3);
+    Result::Result<TestEnum, int> evenMoreOk = Result::Result(TestEnum::EvenMoreOk, 4);
 
     ASSERT_FALSE(worseError.HasValue());
 
@@ -49,10 +49,47 @@ TEST(Result, EnumTest)
     ASSERT_EQ(evenMoreOk.Value(), 4);
 }
 
+TEST(Result, IntTest)
+{
+    Result::Result<int, int> worseError = Result::Result(-2, 0);
+    Result::Result<int, int> error = Result::Result(-1, 0);
+    Result::Result<int, int> ok = Result::Result(0, 2);
+    Result::Result<int, int> stillOk = Result::Result(1, 3);
+    Result::Result<int, int> evenMoreOk = Result::Result(2, 4);
+
+    ASSERT_FALSE(worseError.HasValue());
+
+    ASSERT_FALSE(error.HasValue());
+
+    ASSERT_TRUE(ok.HasValue());
+    ASSERT_EQ(*ok, 2);
+    ASSERT_EQ(ok.Value(), 2);
+
+    ASSERT_TRUE(stillOk.HasValue());
+    ASSERT_EQ(*stillOk, 3);
+    ASSERT_EQ(stillOk.Value(), 3);
+
+    ASSERT_TRUE(evenMoreOk.HasValue());
+    ASSERT_EQ(*evenMoreOk, 4);
+    ASSERT_EQ(evenMoreOk.Value(), 4);
+}
+
+TEST(Result, BoolTest)
+{
+    Result::Result<bool, int> error = Result::Result(false, 0);
+    Result::Result<bool, int> ok = Result::Result(true, 1);
+
+    ASSERT_FALSE(error.HasValue());
+
+    ASSERT_TRUE(ok.HasValue());
+    ASSERT_EQ(*ok, 1);
+    ASSERT_EQ(ok.Value(), 1);
+}
+
 TEST(Result, ObjectAccess)
 {
-    Result::Result<TestClass> testObjectError = Result::Result<TestClass>(TestEnum::Error, Result::nullresult);
-    Result::Result<TestClass> testObjectOk = Result::Result<TestClass>(TestEnum::Ok, TestClass());
+    Result::Result<TestEnum, TestClass> testObjectError = Result::Result<TestEnum, TestClass>(TestEnum::Error, Result::nullresult);
+    Result::Result<TestEnum, TestClass> testObjectOk = Result::Result<TestEnum, TestClass>(TestEnum::Ok, TestClass());
 
     ASSERT_FALSE(testObjectError.HasValue());
 
